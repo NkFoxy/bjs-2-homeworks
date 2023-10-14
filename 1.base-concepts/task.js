420b6580678954c4852156910e7eb8e1a1e8a98b
Natalia Vilkova <nkfoxy@mail.ru>
2023-10-14 21:03:51 +0300

    correct task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,7 +4,7 @@ function solveEquation(a, b, c) {
 	let arr = [];
 	let d = Math.pow(b, 2) - 4 * a * c;
 	if (d = 0) {
-		arr = -b / (2 * a);
+		arr = (-b / (2 * a));
 		console.log("Дискриминант равен 0, уравнение имеет один корень", arr)
 	} else if (d < 0) {
 		arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)]
