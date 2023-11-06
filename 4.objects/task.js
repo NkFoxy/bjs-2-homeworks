40084eaa54cea55cf035e6877a7b7f6ce1f25770
Natalia Vilkova <nkfoxy@mail.ru>
2023-11-06 20:49:47 +0300

    correct task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -5,37 +5,37 @@ function Student(name, gender, age) {
     this.marks = [];
 }
 
-let student1 = new Student ("Василиса", "женский", 19);
+let student1 = new Student("Василиса", "женский", 19);
 let student2 = new Student("Артём", "мужской", 25);
 
 Student.prototype.setSubject = function (subjectName) {
-  this.subjectName = subjectName;
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  if (Student.marks < 1) {
-    console.log("Студент отчислен");
-  }
-  else {
-    this.marks = marks;
-  }
-}
-
-Student.prototype.getAverage = function () {
-    if (Student.marks < 1) {
-        return 0;
-    } else {
-        for (let item of Object.entries(Student.marks)){
-            item++;
-            let average = average + marks[1];
-        }
-        getAverage =  average / item;
+    if (this.hasOwnProperty("marks")) {
+        this.marks = [...this.marks, ...marks]
     }
 }
 
+Student.prototype.getAverage = function () {
+    if (!this.hasOwnProperty('marks')) return 0;
+
+    let average = this.marks.reduce((acc, item, index, marks) => {
+        acc += item;
+        if (index === marks.length - 1) {
+            return acc / marks.length
+        }
+        return acc;
+    }, 0);
+debugger;
+    return average;
+
+}
+
 Student.prototype.exclude = function (reason) {
-  delete Student.subject;
-  delete Student.marks;
-  this.excluded = reason;
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
 
