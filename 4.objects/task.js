305361c9b49dbe1932739b29f204eae6e06990ca
Natalia Vilkova <nkfoxy@mail.ru>
2023-11-06 19:01:35 +0300

    correct task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -9,11 +9,11 @@ let student1 = new Student ("Василиса", "женский", 19);
 let student2 = new Student("Артём", "мужской", 25);
 
 Student.prototype.setSubject = function (subjectName) {
-  subject = subjectName;
+  this.subjectName = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  if (Student.prototype.addMarks < 1) {
+  if (Student.marks < 1) {
     console.log("Студент отчислен");
   }
   else {
@@ -22,12 +22,12 @@ Student.prototype.addMarks = function (...marks) {
 }
 
 Student.prototype.getAverage = function () {
-    if (Student.prototype.addMarks < 1) {
+    if (Student.marks < 1) {
         return 0;
     } else {
         for (let item of Object.entries(Student.marks)){
             item++;
-            let average = average + marks;
+            let average = average + marks[1];
         }
         getAverage =  average / item;
     }
