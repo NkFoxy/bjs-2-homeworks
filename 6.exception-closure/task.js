6e64bcf53cd61870ff2a4f7504d7c329d83a36a6
Natalia Vilkova <nkfoxy@mail.ru>
2023-12-03 23:39:38 +0300

    create task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,64 @@
-﻿
+﻿function parseCount(value) {
+    let b = Number.parseFloat(value);
+    if (Number.isNaN(b)) {
+        const error = new Error("Невалидное значение");
+        throw error;
+    }
+    return b;
+}
+
+function validateCount(value) {
+    try {
+        let val = parseCount(value);
+        return val;
+    } catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        this.#check(a, b, c);
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+    }
+
+    #check(a, b, c) {
+        if (((a + b) < c) || ((b + c) < a) || ((a + c) < b)) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        let p = this.perimeter / 2;
+        return Number((Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3));
+    }
+
+}
+
+class ErrorInfo {
+    get perimeter() {
+        return "Ошибка! Треугольник не существует";
+    }
+
+    get area() {
+        return "Ошибка! Треугольник не существует";
+    }
+}
+
+function getTriangle (a, b, c) {
+    try {
+        return new Triangle (a, b, c)
+    } catch {
+        return new ErrorInfo();
+    }
+
+}
+
+
